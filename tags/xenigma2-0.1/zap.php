d486e18cf7ebac0426843e22869a2dbb6c33d789
ops@iki.fi <ops@iki.fi@4a23223e-2e5e-7c42-0da4-49bbd8a4b440>
2011-11-07 22:45:33 +0000

    Release xenigma2-0.1

---
+++ tags/xenigma2-0.1/zap.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Copyright (C) 2011 Olli Savia
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * $Id$
+ */
+
+require_once ('Enigma2WebInterface.php');
+require_once ('configuration.php');
+
+$webif = new E2WebInterface($ip_addr, $http_port, $login);
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
+?>
+<rss version="2.0" xmlns:media="http://purl.org/dc/elements/1.1/" xmlns:dc="http://purl.org/dc/elements/1.1/">
+
+
+  // nothing here yet
+  
+</rss>
