cc36faccc31e61a982af86822cfd34cbb6704661
ops <ops@iki.fi>
2011-11-07 19:56:49 +0000

    Refactored XML_Loader out from E2WebIF

--- Enigma2WebInterface.php
+++ Enigma2WebInterface.php
@@ -20,6 +20,8 @@
  * $Id$
  */
 
+require_once ('XML_Loader.php');
+
 class E2event {
   var $id;
   var $start;
@@ -46,22 +48,17 @@ class E2webInterface {
   var $ip_addr;
   var $http_port;
   var $login;
-  var $curl;
+  var $xml_loader;
 
   function __construct($ip_addr, $http_port, $login) {
     $this->ip_addr = $ip_addr;
     $this->http_port = $http_port;
     $this->login = $login;
-
-    // Create curl resource
-    $this->curl = curl_init();
-
-    // Return the transfer as a string
-    curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+    $this->xml_loader = new XML_Loader();
   }
+
   function __destruct() {
-    // Close curl resource to free up system resources
-    curl_close($this->curl);
+    $this->xml_loader = 0;
   }
 
   function encodeServiceReference($ref) {
@@ -71,7 +68,7 @@ class E2webInterface {
   function buildServiceList($xmldoc) {
     $services = array();
     $serviceitem = new E2service();
-    foreach (simplexml_load_string($xmldoc) as $service) {
+    foreach ($xmldoc as $service) {
       $serviceitem->servicereference = $this->encodeServiceReference((string)$service->e2servicereference);
       $serviceitem->servicename = (string)$service->e2servicename;
       $services[] = clone($serviceitem);
@@ -81,22 +78,22 @@ class E2webInterface {
 
   function loadBouquets() {
     $url = "$this->ip_addr:$this->http_port/web/getservices";
-    $doc = $this->retrieve($url);
-    return $this->buildServiceList($doc);
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    return $this->buildServiceList($xmldoc);
   }
 
   function loadServices($serviceReference) {
     $url = "$this->ip_addr:$this->http_port/web/getservices?sRef=$serviceReference";
-    $doc = $this->retrieve($url);
-    return $this->buildServiceList($doc);
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    return $this->buildServiceList($xmldoc);
   }
 
   function loadPlayableList($serviceReference) {
     $url = "$this->ip_addr:$this->http_port/web/servicelistplayable?sRef=$serviceReference";
-    $xmldoc = $this->retrieve($url);
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
     $services = array();
     $playable = new E2playable();
-    foreach (simplexml_load_string($xmldoc) as $event) {
+    foreach ($xmldoc as $event) {
       $playable->servicereference = $this->encodeServiceReference((string)$event->e2servicereference);
       if($event->e2isplayable == 'True')
         $playable->isplayable = "1";
@@ -110,7 +107,7 @@ class E2webInterface {
   function buildEPGList($xmldoc) {
     $services = array();
     $serviceevent = new E2event();
-    foreach (simplexml_load_string($xmldoc) as $event) {
+    foreach ($xmldoc as $event) {
       $serviceevent->id = (int)$event->e2eventid;
       $serviceevent->start = (int)$event->e2eventstart;
       $serviceevent->duration = (int)$event->e2eventduration;
@@ -127,37 +124,34 @@ class E2webInterface {
 
   function loadEPG($serviceReference) {
     $url = "$this->ip_addr:$this->http_port/web/epgservice?sRef=$serviceReference";
-    $doc = $this->retrieve($url);
-    return $this->buildEPGList($doc);
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    return $this->buildEPGList($xmldoc);
   }
 
   function loadEPGnow($bouquetReference) {
     $url = "$this->ip_addr:$this->http_port/web/epgnow?bRef=$bouquetReference";
-    $doc = $this->retrieve($url);
-    return $this->buildEPGList($doc);
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    return $this->buildEPGList($xmldoc);
   }
 
   function zap($serviceReference) {
     $url = "$this->ip_addr:$this->http_port/web/zap?sRef=$serviceReference";
-    $doc = $this->retrieve($url);
-    $result = simplexml_load_string($doc);
-    if ($result->e2state == 'True')
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    if ($xmldoc->e2state == 'True')
       return true;
     return false;
   }
 
   function message($text, $type, $timeout) {
     $url = "$this->ip_addr:$this->http_port/web/message?text=$text&type=$type&timeout=$timeout";
-    $doc = $this->retrieve($url);
-    $result = simplexml_load_string($doc);
-    return $result->e2resulttext;
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    return $xmldoc->e2resulttext;
   }
 
   function isPlayable($serviceReference) {
     $url = "$this->ip_addr:$this->http_port/web/serviceplayable?sRef=$serviceReference";
-    $doc = $this->retrieve($url);
-    $result = simplexml_load_string($doc);
-    if ($result->e2isplayable == 'True')
+    $xmldoc = $this->xml_loader->Retrieve($url, $this->login);
+    if ($xmldoc->e2isplayable == 'True')
       return true;
     return false;
   }
@@ -188,20 +182,6 @@ class E2webInterface {
 
     socket_close($socket);
     return $retval;
-  }
-
-  function retrieve($url) {
-    // Set usename & password
-    if($this->login != '')
-      curl_setopt($this->curl, CURLOPT_USERPWD, $this->login);
-
-    // Set URL
-    curl_setopt($this->curl, CURLOPT_URL, $url);
-
-    // $output contains the output string
-    $output = curl_exec($this->curl);
-
-    return $output;
   }
 }
 
